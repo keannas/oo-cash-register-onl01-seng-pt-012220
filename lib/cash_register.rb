850646bf22418aaefb09049ef4ac38f4a0e63a95
Keanna Salter <keannasalter91@gmail.com>
2020-03-02 20:08:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
-
+class CashRegister
+  
+  attr_accessor :total, :add_item, :apply_discount, :items, :void_last_transcation
+  
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      whiile i<quantity
+      
